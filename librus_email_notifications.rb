72dd02eab3173c252f5dccddc5a303bcbed7a7a0
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2015-10-29 15:44:17 +0000

    Printing to the log file and console + run lock to prevent launching of multiple instances from cron

--- librus_email_notifications.rb
+++ librus_email_notifications.rb
@@ -10,8 +10,28 @@ module LibrusEmailNotifications
     log_dir = "log"
     df = "%Y-%m-%d %H:%M:%S"
 
+    Dir.mkdir(data_dir) unless File.exists?(data_dir)
+    Dir.mkdir(log_dir) unless File.exists?(log_dir)
+
+    def LibrusEmailNotifications::log(message)
+        log_entry = "#{DateTime.now.strftime("%Y-%m-%d %H:%M:%S")} #{message}"
+        puts log_entry
+        File.open("log/len.log","a") { |f| f.puts(log_entry) }
+    end
+
+    if File.exists?("lockfile")
+        log "Another instance is already running. Aborting."
+        abort
+    end
+
+    at_exit do
+        File.delete("lockfile")
+    end
+
+    File.open("lockfile","w") {}
+
     if ARGV.length != 6
-            puts "Usage: ruby librus.rb <librus_user> <librus_password> <smtp_host> <smtp_email> <smtp_user> <smtp_password>"
+            log "Usage: ruby librus.rb <librus_user> <librus_password> <smtp_host> <smtp_email> <smtp_user> <smtp_password>"
             abort
     end
 
@@ -25,9 +45,6 @@ module LibrusEmailNotifications
 
     data_file = "#{data_dir}/#{librus_user}-data.txt"
 
-    Dir.mkdir(data_dir) unless File.exists?(data_dir)
-    Dir.mkdir(log_dir) unless File.exists?(log_dir)
-
     if !File.exists?(data_file)
         File.open(data_file,"w"){}
     end
@@ -40,41 +57,41 @@ module LibrusEmailNotifications
 
     Capybara.default_driver = :poltergeist
 
-    puts "Browsing to the login page"
+    log "Browsing to the login page"
 
     Capybara.page.visit('http://synergia.librus.pl/loguj')
 
-    puts "Logging into Librus"
+    log "Logging into Librus"
 
     Capybara.page.fill_in('login', :with => librus_user)
     Capybara.page.fill_in('passwd', :with => librus_password)
     Capybara.page.find(:xpath, "//input[@name='loguj']").click
 
-    puts "Waiting 10 seconds"
+    log "Waiting 10 seconds"
 
     sleep 10
 
-    puts "Navigating to messages"
+    log "Navigating to messages"
 
     Capybara.page.find(:xpath, "//a[@id='icon-wiadomosci']").click
 
-    puts "Scanning links"
+    log "Scanning links"
     links = Capybara.page.all(:xpath, "//a[starts-with(@href, '/wiadomosci/')]")
     hrefs = links.map{|x| x[:href]}.select{ |x| /.*\/wiadomosci\/\d+\/\d+\/(.*)/.match(x) != nil}
 
     ids = hrefs.map{|href| /.*\/wiadomosci\/\d+\/\d+\/(.*)/.match(href)[1].to_i}.uniq.sort
 
-    puts "Found #{ids.length} links"
+    log "Found #{ids.length} links"
 
     sent_messages_ids = File.read(data_file).split(/\n/).map{|x| x.to_i}
 
     messages_that_need_to_be_processed = ids - sent_messages_ids
 
-    puts "#{messages_that_need_to_be_processed.length} of them have to be processed"
+    log "#{messages_that_need_to_be_processed.length} of them have to be processed"
 
     messages_that_need_to_be_processed.each do |id|
 
-            puts "Processing message #{id}"
+            log "Processing message #{id}"
 
             link_ending = "/wiadomosci/1/5/#{id}"
             link = Capybara.page.find(:xpath, "//a[starts-with(@href, '#{link_ending}')]")
@@ -92,7 +109,7 @@ module LibrusEmailNotifications
 
             smtp_start_time = DateTime.now
 
-            smtp_sender.send_message(sender_display_name, "Sebastian.Celejewski@wp.pl", topic, text)
+            smtp_sender.send_message(sender_display_name, "Sebastian.Celejewski@wp.pl;Ag.Celejewska@wp.pl", topic, text)
 
             smtp_end_time = DateTime.now
 
@@ -102,11 +119,11 @@ module LibrusEmailNotifications
 
             File.open("log/smtp.log","a") {|f| f.puts "#{smtp_start_time.strftime(df)};#{smtp_end_time.strftime(df)};#{smtp_duration}" }
 
-            puts "Coming back to messages list"
+            log "Coming back to messages list"
 
             link = Capybara.page.find(:xpath, "//a[starts-with(@href, '/wiadomosci/5')]")
             link.click
     end
 
-    puts "Done."
+    log "Done."
 end
